59cd216a55ca462049c254cf4d49166518c73314
Muhammad Farhan Al Rahman <87657233+mfarchan-arm@users.noreply.github.com>
2024-07-13 15:37:50 +0700

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/landing.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "dashboard.html",
+    }),
+  ],
+  devServer: {
+    watchFiles: ["./src/*.html"],
+    port: 8080,
+    open: true,
+    hot: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+};
